7bf540011c7531785e8ca564f86534404299ab98
guojc <guojc@meixing.com>
2024-04-30 14:16:00 +0800

    替换原有Application

--- DecryptionLib/src/main/java/com/magicianguo/decryptionlib/ProxyApp.java
+++ DecryptionLib/src/main/java/com/magicianguo/decryptionlib/ProxyApp.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProxyApp extends Application {
+    private Application delegate;
     private String app_name;
 
     @Override
@@ -66,6 +67,12 @@ public class ProxyApp extends Application {
         }
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        bindOriginApplication();
+    }
+
     private void getMetaData() {
         try {
             ApplicationInfo applicationInfo = getPackageManager().getApplicationInfo(
@@ -93,5 +100,100 @@ public class ProxyApp extends Application {
         System.arraycopy(dexElements, 0, newElements, 0, dexElements.length);
         System.arraycopy(addElements, 0, newElements, dexElements.length, addElements.length);
         dexElementsField.set(pathList, newElements);
+    }
+
+    private void bindOriginApplication() {
+        if (TextUtils.isEmpty(app_name)) {
+            return;
+        }
+        try {
+            //1. 得到 attachBaseContext(context) 传入的上下文 ContextImpl
+            Context baseContext = getBaseContext();
+            //2. 拿到真实 APK APPlication 的 class
+            Class<?> delegateClass = Class.forName(app_name);
+            //3. 反射实例化，其实 Android 中四大组件都是这样实例化的。
+            delegate = (Application) delegateClass.newInstance();
+
+            //3.1 得到 Application attach() 方法 也就是最先初始化的
+            Method attach = Application.class.getDeclaredMethod("attach", Context.class);
+            attach.setAccessible(true);
+            //执行 Application#attach(Context)
+            //3.2 将真实的 Application 和假的 Application 进行替换。想当于自己手动控制 真实的 Application 生命周期
+            attach.invoke(delegate, baseContext);
+
+
+//        ContextImpl---->mOuterContext(app)   通过Application的attachBaseContext回调参数获取
+            //4. 拿到 Context 的实现类
+            Class<?> contextImplClass = Class.forName("android.app.ContextImpl");
+            //4.1 获取 mOuterContext Context 属性
+            Field mOuterContextField = contextImplClass.getDeclaredField("mOuterContext");
+            mOuterContextField.setAccessible(true);
+            //4.2 将真实的 Application 交于 Context 中。这个根据源码执行，实例化 Application 下一个就行调用 setOuterContext 函数，所以需要绑定 Context
+            //  app = mActivityThread.mInstrumentation.newApplication(
+            //                    cl, appClass, appContext);
+            //  appContext.setOuterContext(app);
+            mOuterContextField.set(baseContext, delegate);
+
+//        ActivityThread--->mAllApplications(ArrayList)       ContextImpl的mMainThread属性
+            //5. 拿到 ActivityThread 变量
+            Field mMainThreadField = contextImplClass.getDeclaredField("mMainThread");
+            mMainThreadField.setAccessible(true);
+            //5.1 拿到 ActivityThread 对象
+            Object mMainThread = mMainThreadField.get(baseContext);
+
+//        ActivityThread--->>mInitialApplication
+            //6. 反射拿到 ActivityThread class
+            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+            //6.1 得到当前加载的 Application 类
+            Field mInitialApplicationField = activityThreadClass.getDeclaredField("mInitialApplication");
+            mInitialApplicationField.setAccessible(true);
+            //6.2 将 ActivityThread 中的 Applicaiton 替换为 真实的 Application 可以用于接收相应的声明周期和一些调用等
+            mInitialApplicationField.set(mMainThread, delegate);
+
+
+//        ActivityThread--->mAllApplications(ArrayList)       ContextImpl的mMainThread属性
+            //7. 拿到 ActivityThread 中所有的 Application 集合对象，这里是多进程的场景
+            Field mAllApplicationsField = activityThreadClass.getDeclaredField("mAllApplications");
+            mAllApplicationsField.setAccessible(true);
+            ArrayList<Application> mAllApplications = (ArrayList<Application>) mAllApplicationsField.get(mMainThread);
+            //7.1 删除 ProxyApplication
+            mAllApplications.remove(this);
+            //7.2 添加真实的 Application
+            mAllApplications.add(delegate);
+
+//        LoadedApk------->mApplication                      ContextImpl的mPackageInfo属性
+            //8. 从 ContextImpl 拿到 mPackageInfo 变量
+            Field mPackageInfoField = contextImplClass.getDeclaredField("mPackageInfo");
+            mPackageInfoField.setAccessible(true);
+            //8.1 拿到 LoadedApk 对象
+            Object mPackageInfo = mPackageInfoField.get(baseContext);
+
+            //9 反射得到 LoadedApk 对象
+            //    @Override
+            //    public Context getApplicationContext() {
+            //        return (mPackageInfo != null) ?
+            //                mPackageInfo.getApplication() : mMainThread.getApplication();
+            //    }
+            Class<?> loadedApkClass = Class.forName("android.app.LoadedApk");
+            Field mApplicationField = loadedApkClass.getDeclaredField("mApplication");
+            mApplicationField.setAccessible(true);
+            //9.1 将 LoadedApk 中的 Application 替换为 真实的 Application
+            mApplicationField.set(mPackageInfo, delegate);
+
+            //修改ApplicationInfo className   LooadedApk
+
+            //10. 拿到 LoadApk 中的 mApplicationInfo 变量
+            Field mApplicationInfoField = loadedApkClass.getDeclaredField("mApplicationInfo");
+            mApplicationInfoField.setAccessible(true);
+            //10.1 根据变量反射得到 ApplicationInfo 对象
+            ApplicationInfo mApplicationInfo = (ApplicationInfo) mApplicationInfoField.get(mPackageInfo);
+            //10.2 将我们真实的 APPlication ClassName 名称赋值于它
+            mApplicationInfo.className = app_name;
+
+            //11. 执行 代理 Application onCreate 生命周期
+            delegate.onCreate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
